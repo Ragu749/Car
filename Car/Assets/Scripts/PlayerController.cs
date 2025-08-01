812f26adbd06c85c05c42028ec0ab2b178bb7871
raguram <raguram90@aol.com>
2025-08-01 18:14:56 +0100

    Updated player controller

--- Car/Assets/Scripts/PlayerController.cs
+++ Car/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
+    private float speed = 40;
+    private float turnSpeed = 50;
+    private InputAction moveAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveValue[1]);
+
+        if (moveValue[1] > 0) transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveValue[0]);
+        if (moveValue[1] < 0) transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * -moveValue[0]);
     }
 }
