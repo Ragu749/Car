01cc6347db08b3152693b57ce3a5c9654a434b0c
raguram <raguram90@aol.com>
2025-08-03 17:56:11 +0100

    Fixed and completed plane challenge

--- Car/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Car/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,29 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    private InputAction moveAction;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        verticalInput = moveAction.ReadValue<Vector2>()[1];
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
-    }
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    } 
 }
---
+++ Car/Assets/Challenge 1/Scripts/Propeller.cs
@@ -0,0 +1,18 @@
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Propeller : MonoBehaviour
+{
+    private float propellerSpeed = 1000;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propellerSpeed * Time.deltaTime);
+    }
+}
